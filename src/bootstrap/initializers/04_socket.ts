11c4302d8156635f9a33d4388bc47f7fb98f6af4
Jasper Bernales <bernalesjasper@gmail.com>
2020-05-31 19:31:22 +0800

    fix

--- src/bootstrap/initializers/04_socket.ts
+++ src/bootstrap/initializers/04_socket.ts
@@ -6,13 +6,6 @@ type Socket = socketIo.Socket & { user_id: string; username: string }
 export default function initializeSocket({ server }: InitializerContext) {
   const io = socketIo.listen(server.server)
   const DB = serviceLocator.get('DB')
-  io.use((socket: Socket, next) => {
-    if (!socket.user_id) {
-      throw new Error('Unauthorized')
-    }
-    next()
-  })
-
   io.on('connection', (socket: Socket) => {
     socket.on('joinUser', (data: { username: string; user_id: string }) => {
       socket.user_id = data.user_id
