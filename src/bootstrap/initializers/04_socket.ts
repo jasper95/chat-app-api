89d7a5d285cfaafedbe25b910f076ce6c766fe0d
Jasper Bernales <bernalesjasper@gmail.com>
2020-05-31 19:03:00 +0800

    joinUser

--- src/bootstrap/initializers/04_socket.ts
+++ src/bootstrap/initializers/04_socket.ts
@@ -10,23 +10,10 @@ export default function initializeSocket({ server }: InitializerContext) {
   const io = socketIo.listen(server.server)
   const DB = serviceLocator.get('DB')
 
-  io.use((socket: Socket, next) => {
-    const { cookie: cookie_header } = socket.request.headers
-    logger.info('Cookie: [%s]', util.inspect(cookie))
-    if (cookie_header) {
-      const { access_token } = cookie.parse(cookie_header)
-      if (access_token) {
-        const { user_id, username } = jwt.verify(access_token, process.env.AUTH_SECRET) as AuthSession
-        socket.user_id = user_id
-        socket.username = username
-      }
-    }
-    next()
-  })
-
   io.on('connection', (socket: Socket) => {
-    socket.broadcast.emit('userJoined', {
-      username: socket.username,
+    socket.on('joinUser', (data: { username: string; user_id: string }) => {
+      socket.user_id = data.user_id
+      socket.username = data.username
     })
     socket.on('newMessage', async (data, cb) => {
       const response = await DB.insert('message', data)
